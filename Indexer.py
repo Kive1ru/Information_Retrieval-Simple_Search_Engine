f7813765f0815b084d09bcb54caba311191f84b0
Kylin Guo <42748563+Kive1ru@users.noreply.github.com>
2022-03-07 17:32:05 -0800

    Update Indexer.py

--- Indexer.py
+++ Indexer.py
@@ -1,13 +1,13 @@
 # 15786064(Zijian Chen), 13713641(Qingshuang Su), 70518431(Lingxin Li), 90277259(Jiahao(Kylin) Guo)
+import os
+import math
+import json
+from simhash import Simhash
+from bs4 import BeautifulSoup
+from collections import defaultdict
 from bs4 import BeautifulSoup
 from nltk.tokenize import word_tokenize
 from nltk.stem.snowball import SnowballStemmer
-import json
-from collections import defaultdict
-import os
-from urllib.parse import urldefrag
-import math
-from simhash import Simhash
 
 
 indexer_dict = defaultdict(dict)
