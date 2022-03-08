495f175a0acf44d813f1ff0dd4efe40061e9fd0d
Kylin Guo <42748563+Kive1ru@users.noreply.github.com>
2022-03-07 17:14:15 -0800

    Add files via upload

---
+++ Indexer.py
@@ -0,0 +1,206 @@
+# 15786064(Zijian Chen), 13713641(Qingshuang Su), 70518431(Lingxin Li), 90277259(Jiahao(Kylin) Guo)
+from bs4 import BeautifulSoup
+from nltk.tokenize import word_tokenize
+from nltk.stem.snowball import SnowballStemmer
+import json
+from collections import defaultdict
+import os
+from urllib.parse import urldefrag
+import math
+from simhash import Simhash
+
+
+indexer_dict = defaultdict(dict)
+numPage = 0
+docID = 0
+docUrl = dict()
+unique_url = set()
+unique_word = 0
+splitCount = 0
+comparehash = set()
+
+def tokenize(content):
+    stemmer = SnowballStemmer("english")
+    return [stemmer.stem(word) for word in word_tokenize(content) if word.isalnum()]
+
+
+def indexer(content, types, docID):
+    global Posting
+    global indexer_dict
+    if types == 1:  # bold and strong
+        for token in content:
+            if not token in indexer_dict:
+                indexer_dict[token][docID] = 1.5
+            elif not docID in indexer_dict[token]:
+                indexer_dict[token][docID] = 1.5
+            else:
+                indexer_dict[token][docID] += 1.5       
+    elif types == 2: # heading
+        for token in content:
+            if not token in indexer_dict:
+                indexer_dict[token][docID] = 2
+            elif not docID in indexer_dict[token]:
+                indexer_dict[token][docID] = 2
+            else:
+                indexer_dict[token][docID] += 2
+    elif types == 3: # title
+        for token in content:
+            if not token in indexer_dict:
+                indexer_dict[token][docID] = 2.5
+            elif not docID in indexer_dict[token]:
+                indexer_dict[token][docID] = 2.5
+            else:
+                indexer_dict[token][docID] += 2.5
+    else: # frequency
+        for token in content:
+            if not token in indexer_dict:
+                indexer_dict[token][docID] = 1
+            elif not docID in indexer_dict[token]:
+                indexer_dict[token][docID] = 1
+            else:
+                indexer_dict[token][docID] += 1
+
+
+def simhash_diff(hash_1, hash_2):  #simhash comparison function from #https://algonotes.readthedocs.io/en/latest/Simhash.html
+    """calcuate the difference from two simhash values.
+    """
+    x = (hash_1 ^ hash_2) & ((1 << 64) - 1)
+    ans = 0
+    while x:
+        ans += 1
+        x &= x - 1
+    return ans
+
+
+def readjson(file_path):
+    global docID
+    global numPage
+    global splitCount
+    f = open(file_path)
+    js = json.load(f)
+    soup = BeautifulSoup(js['content'], 'html.parser')
+    if len(soup.get_text()) <= 75000:
+        pagehash = Simhash(soup.get_text())
+        button = False
+        if not comparehash: #add the first hashvalue to comparehash
+            comparehash.add(pagehash.value)
+        else:
+            for i in comparehash:
+                if simhash_diff(pagehash.value,i) <= 3:
+                    button = True
+        if not button:
+            comparehash.add(pagehash.value)
+            docID += 1
+            splitCount += 1
+            docUrl[docID] = js['url']
+            for b in soup.find_all(['b', 'strong']):
+                indexer(tokenize(b.get_text()), 1, docID)
+            for h in soup.find_all(['h1', 'h2', 'h3']):
+                indexer(tokenize(h.get_text()), 2, docID)
+            for t in soup.find_all('title'):
+                indexer(tokenize(t.get_text()), 3, docID)
+            indexer(tokenize(soup.get_text()), 0, docID)
+    numPage += 1
+
+
+          
+def get_files(root):
+    files = os.listdir(root)
+    paths = []
+    for f in files:
+        absolute_path = os.path.join(root,f)
+        if os.path.isdir(absolute_path):
+            paths.extend(get_files(absolute_path))
+        else:
+            if absolute_path.endswith('.json'):
+                if not absolute_path.endswith('docID.json'):
+                    paths.append(absolute_path)
+    return paths
+
+
+def storeIndexer(root):
+    global indexer_dict
+    global docUrl
+    global unique_word
+    directory = "TEST"
+    path = os.path.join(root, directory)
+    if not os.path.exists(path):
+        os.mkdir(path)
+    docID_path = os.path.join(path, "docID.json")
+    with open(docID_path, "w") as dd:
+        json.dump(docUrl, dd, indent = 5)
+    for key, value in indexer_dict.items():
+        subdir = key[0]
+        tPath = os.path.join(path, subdir)
+        if not os.path.exists(tPath):
+            os.mkdir(tPath)
+        if key == "aux":
+            tfilename = os.path.join(tPath, "aux_.json")
+        else:
+            tfilename = os.path.join(tPath, key + ".json")
+        if not os.path.isfile(tfilename):
+            with open(tfilename, "w", encoding = 'utf-8') as idx1:
+                json.dump(value, idx1, indent=4)
+        else:
+            with open(tfilename, "r", encoding = 'utf-8') as idx2:
+                indexers = json.load(idx2)
+            os.remove(tfilename)
+            with open(tfilename, "w", encoding = 'utf-8') as idx3:
+                indexers.update(value)
+                json.dump(indexers, idx3, indent=4)
+    unique_word += len(indexer_dict.keys())
+    indexer_dict = defaultdict(dict)
+
+
+def grade(path):
+    global docUrl
+    with open(path, "r", encoding = 'utf-8') as idx4:
+        termDict = json.load(idx4)
+    for doc in termDict:
+        termDict[doc] = (1 + math.log(termDict[doc])) * math.log(len(docUrl.keys()) / len(termDict))
+    os.remove(path)
+    with open(path, "w", encoding = 'utf-8') as idx5:
+        json.dump(termDict, idx5, indent=4)
+
+
+def report(storeRoot):
+    global numPage
+    global docID
+    global unique_word
+    reportTxt = os.path.join(storeRoot, 'report.txt')
+    with open(reportTxt,"a") as rp:
+        rp.write (f"The number of pages in the dataset: {numPage}")
+        rp.write("\n")
+        rp.write (f"The number of indexed documents: {docID}")
+        rp.write("\n")
+        rp.write (f"unique words: {unique_word}")
+        rp.write("\n")
+
+     
+def run(root, storeRoot):
+    global splitCount
+    file_paths = get_files(root)
+    splitCount = 0
+    for p in file_paths:
+        print(p)
+        readjson(p)
+        if splitCount == 14000:
+            storeIndexer(storeRoot)
+            splitCount = 0
+    storeIndexer(storeRoot)
+    print('finish Dividing')
+    index_path = os.path.join(storeRoot, 'TEST')
+    result_paths = get_files(index_path)
+    print('start tf-idf')
+    for p in result_paths:
+        print(p)
+        grade(p)
+    report(storeRoot)
+    print('finish')
+
+
+if __name__ == '__main__':
+    root = 'B:\CS 121\Assignment3M3\DEV'
+    storeRoot = 'B:\CS 121\Assignment3M3'
+    run(root, storeRoot)
+
---
+++ Searcher.py
@@ -0,0 +1,105 @@
+#15786064(Zijian Chen), 13713641(Qingshuang Su), 70518431(Lingxin Li), 90277259(Jiahao(Kylin) Guo)
+from nltk.tokenize import word_tokenize
+from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
+from collections import defaultdict
+import json
+import time
+import os
+
+
+docid = dict()
+
+
+def read_docID(root):  # load the docID dict 
+    global docid
+    json_file = os.path.join(root, 'docID.json')
+    with open(json_file, 'r', encoding='utf-8') as d:
+        docid = json.load(d)
+
+
+def Load_json(query_token):  # get the dict item of sepecific token term
+    global root
+    if query_token == "aux":
+        dir_file = os.path.join(root, query_token[0])
+        json_file = os.path.join(dir_file, query_token + '_.json')
+    else:
+        dir_file = os.path.join(root, query_token[0])
+        json_file = os.path.join(dir_file, query_token + '.json')
+    try:
+        with open(json_file, 'r', encoding='utf-8') as idx:
+            indexes = json.loads(idx.read())
+        return indexes
+    except:
+        return {}
+
+
+def tokenize(content): # tokenize query
+    stemmer = SnowballStemmer("english")
+    return [stemmer.stem(word) for word in word_tokenize(content) if word.isalnum()]
+
+
+def Query_search(query):  # get a list of query terms associated with their postings
+    relst = []
+    query_lst = tokenize(query)
+    for qtoken in query_lst:
+        query_dic = Load_json(qtoken)
+        relst.append(query_dic)
+    relst = sorted(relst, key = lambda x: len(x))
+    return relst
+
+
+def Merge_query(query_lst): # find intersection among query terms' postings
+    common_posting = defaultdict(int)
+    sortID = []
+    if query_lst:
+        comEle = query_lst[0].copy()
+        for query in query_lst:
+            comEle = {x:0 for x in comEle if x in query}
+        for ele in comEle:
+            for query in query_lst:
+                common_posting[ele] += query[ele]
+        sortID = sorted(common_posting.keys(), key = lambda x: -common_posting[x])
+    return sortID
+
+
+def RankTop(sortPost): # print the top 5 urls
+    global docid
+    check = 0
+    while check < len(sortPost):
+        if check == 5:
+            break
+        print(docid[sortPost[check]])
+        check += 1
+
+
+def UInterface(root): # text user interface of the search engine
+    count = 0
+    while True:
+        query = input("Query (ENTER Key to exit):\n")
+        start = time.process_time()
+        if query == "":
+            print("EXIT THE SEARCH ENGINE")
+            break
+        if count == 0:
+            read_docID(root)
+            count += 1
+        resultUrl = Merge_query((Query_search(query)))
+        end = time.process_time()
+        if not resultUrl:
+            print("")
+            print("No result found")
+        else:
+            print(f"\nSearch time: {end - start} seconds. Total {len(resultUrl)} Urls are found.")
+            if len(resultUrl) >= 5:
+                print("Top 5 results:")
+            else:
+                print(f"Top {len(resultUrl)} results (results found are less than 5):")
+            RankTop(resultUrl)
+            print("")
+    
+
+if __name__ == '__main__':
+    root = 'B:\CS 121\Assignment3M3\TEST'
+    UInterface(root)
+
